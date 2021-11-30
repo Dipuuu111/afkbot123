2c7450f3324d084a7c7860005d6af29b43dc0389
Dipuuu111 <88713641+Dipuuu111@users.noreply.github.com>
2021-11-30 09:51:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -74,3 +74,28 @@ bot.on('death',function() {
     bot.emit("respawn")
 });
 
+function bindEvents(bot) {
+
+    bot.on('error', function(err) {
+        console.log('Error attempting to reconnect: ' + err.errno + '.');
+        if (err.code == undefined) {
+            console.log('Invalid credentials OR bot needs to wait because it relogged too quickly.');
+            console.log('Will retry to connect in 30 seconds. ');
+            setTimeout(relog, 30000);
+        }
+    });
+
+    bot.on('end', function() {
+        console.log("Bot has ended");
+        // If set less than 30s you will get an invalid credentials error, which we handle above.
+        setTimeout(relog, 30000);  
+    });
+}
+
+function relog() {
+    console.log("Attempting to reconnect...");
+    bot = mineflayer.createBot(options);
+    bindEvents(bot);
+}
+
+bindEvents(bot);
